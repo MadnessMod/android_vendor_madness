52666ee2f40f060695b9ba3a952e580579e7f44b
mordesku <m.przybylski@yproject.pl>
2014-12-12 13:47:29 +0100

    Update product.mk

--- product.mk
+++ product.mk
@@ -1,4 +1,6 @@
 CM_BUILDTYPE := MADNESS
+CM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d%H)-$(CM_BUILDTYPE)$(CM_EXTRAVERSION)-$(CM_BUILD)
+CM_DISPLAY_VERSION := $(CM_VERSION)
 PRODUCT_BOOTANIMATION := vendor/extra/bootanimation.zip
 
 SUPERUSER_EMBEDDED := true
@@ -9,10 +11,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
   ro.cm.releasetype=$(CM_BUILDTYPE) \
   ro.modversion=$(CM_VERSION) \
-  ro.cm.display.version=CM-12-MADNESS
+  ro.cm.display.version=$(CM_DISPLAY_VERSION)\
 
 PRODUCT_COPY_FILES +=  \
     vendor/extra/apps/FDroid.apk:system/app/FDroid/FDroid.apk \
     vendor/extra/apps/DAVDroid.apk:system/app/DAVDroid/DAVDroid.apk \
     vendor/extra/apps/ownCloud.apk:system/app/ownCloud/ownCloud.apk \
-
