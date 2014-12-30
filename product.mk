69e52050fa77ffb9aa73bc74f757b863f40ea971
Mateusz Przybylski <mateusz.przybylski@koditto.pl>
2014-12-30 11:40:37 +0100

    evervolv bravo ics madness

--- product.mk
+++ product.mk
@@ -1,21 +1,12 @@
-MM_VERSION = 1.0-SNAPSHOT-$(shell date -u +%Y%m%d%H)
-CM_BUILDTYPE := MADNESS
-CM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d%H)-$(CM_BUILDTYPE)$(CM_EXTRAVERSION)-$(CM_BUILD)
-CM_DISPLAY_VERSION := $(CM_VERSION)
+MM_VERSION = 1.0-SNAPSHOT-$(shell date -u +%Y%m%d%H)-ICS
+ROM_VERSION := Evervolv-Madness-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d%H)
 PRODUCT_BOOTANIMATION := vendor/extra/bootanimation.zip
-
 SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
 
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.madness.version=$(MM_VERSION) \
-  ro.rommanager.developerid=mordesku \
-  ro.cm.version=$(CM_VERSION) \
-  ro.cm.releasetype=$(CM_BUILDTYPE) \
-  ro.modversion=$(CM_VERSION) \
-  ro.cm.display.version=$(CM_DISPLAY_VERSION)\
+  ro.madness.version=$(MM_VERSION) 
 
 PRODUCT_COPY_FILES +=  \
-    vendor/extra/apps/FDroid.apk:system/app/FDroid/FDroid.apk \
-    vendor/extra/apps/DAVDroid.apk:system/app/DAVDroid/DAVDroid.apk \
-    vendor/extra/apps/ownCloud.apk:system/app/ownCloud/ownCloud.apk \
+    vendor/extra/apps/FDroid.apk:system/app/FDroid.apk \
+    vendor/extra/apps/DAVDroid.apk:system/app/DAVDroid.apk \
+    vendor/extra/apps/ownCloud.apk:system/app/ownCloud.apk \
